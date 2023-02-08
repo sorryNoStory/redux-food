5e7e81bd8de5e6987393fd3f8894c697c692e1c5
Медуша <Medusa@MacBook-Pro-4.local>
2023-02-08 12:16:24 +0800

    first commit

---
+++ src/Components/Cart/CartItem.js
@@ -0,0 +1,23 @@
+import dataDishes from "../../data/dataDishes";
+import { useDispatch } from "react-redux";
+import { removeItemFromCart } from "../../redux/cartSlice";
+
+const CartItem = ({cartItem}) => {
+
+    const dishes = dataDishes.find(item => item.id === cartItem.dishId);
+    const dispatch = useDispatch();
+
+
+    return(
+        <div>
+            <p>{dishes.name}</p>
+            <p>{cartItem.quantity} portion(s)</p>
+            <p>Price: $ {dishes.price * cartItem.quantity}</p>
+            <span onClick={() => {dispatch(removeItemFromCart({cartItemId: cartItem.id}))}}><img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png"/> </span>
+
+            </div>
+    
+    )
+}
+
+export default CartItem;
